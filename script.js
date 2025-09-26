db4fa2aa815b58d1611c1073956bd624090dd520
EdvirgenGabriela <edvirgengsilva@gmail.com>
2025-09-26 00:07:07 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//------------------troca de tamagotchi oval -----------------
+let tamagotchiTelaDeInicio = document.getElementById("tamagotchi-oval");
+let tamagotchiCostas = document.getElementById("tamagotchi-costas");
+
+tamagotchiTelaDeInicio.id = "tela-de-inicio"
+
+function trocarTamagotchiOval() {
+  let tamagotchiFrente = document.createElement("img");
+
+  tamagotchiFrente.src = '/img/tamagotchi-frente-reformatado.gif';
+  
+  tamagotchiFrente.id = "tamagotchi-frente";
+
+  tamagotchiCostas.replaceWith(tamagotchiFrente);
+}
+
+tamagotchiTelaDeInicio.addEventListener('click', trocarTamagotchiOval);
+
+//----------------------------------------------troca pra o tamagotchi retangular-----------------------
+
+let ativarTelaDeJogo = document.getElementById("botao-troca-tela-inicio");
+let telaDeJogo = document.getElementById("tamagotchi");
+let primeiraTela= document.getElementById("primeiraTela");
+
+ function trocarParaTelaDeJogo(){
+  primeiraTela.style.display = "none";
+  telaDeJogo.style.display = "flex";
+  ativarTelaDeJogo;
+ };
+
+ativarTelaDeJogo.addEventListener('click',trocarParaTelaDeJogo);
