f07f4d4a2db8777795021f69e468810dab16a76b
EdvirgenGabriela <edvirgengsilva@gmail.com>
2025-09-29 11:18:11 -0300

    Atualizações nas interações

--- scripts/interacoes.js
+++ scripts/interacoes.js
@@ -1,27 +1,58 @@
-  let botaoComer = document.getElementsByClassName("comer")[0];
-    
-  let imagem = document.getElementById('spriteNormal');
-        let statusTexto = document.getElementById('status');
+let botaoComer = document.getElementsByClassName("comer")[0];
+let botaoCarinho = document.getElementsByClassName("carinho")[0];
+let botaoDormir = document.getElementsByClassName("dormir")[0]
+let imagem = document.getElementById('spriteNormal');
+let statusTexto = document.getElementById('status');
 
-        let imagemOriginalSrc = imagem.src;
-        let novaImagemSrc = '/img/sprites/slime-respirando-mal.gif';
-
+let imagemOriginalSrc = imagem.src;
+let novaImagemSrc = '/img/sprites/slime-respirando-mal.gif';
+let imagemComendoSrc = '/img/sprites/slime-comendo.gif';
+let imagemAmorSrc = '/img/sprites/slime-amor.gif';
+let imagemDormindoSrc = '/img/sprites/slime-dormindo.gif';
    
-        function trocarParaNovaImagem() {
-            imagem.src = novaImagemSrc;
-            statusTexto.textContent = 'A imagem foi trocada automaticamente!';
-            console.log('Imagem alterada após 30 segundos.');
-        }
+function trocarParaSlimeTriste() {
+    imagem.src = novaImagemSrc;
+    statusTexto.textContent = 'A imagem foi trocada automaticamente!';
+    console.log('Imagem alterada após 30 segundos.');
+}
 
       
-        function restaurarImagemOriginal() {
-           
-            imagem.src = imagemOriginalSrc;
-            statusTexto.textContent = 'A imagem original foi restaurada!';
-            console.log('Imagem restaurada pelo usuário.');
+function restaurarImagemOriginal() {       
+    imagem.src = imagemOriginalSrc;
+    statusTexto.textContent = 'A imagem original foi restaurada!';
+    console.log('Imagem restaurada pelo usuário.');
+}
 
-        }
+function trocarParaSlimeComendo() {       
+    imagem.src = imagemComendoSrc;
+    setTimeout(restaurarImagemOriginal, 7000);
+    setTimeout(trocarParaSlimeTriste, 20000); 
+    statusTexto.textContent = 'A imagem original foi restaurada!';
+    console.log('Imagem restaurada pelo usuário.');
+}
 
+
+function trocarParaSlimeCarinho() {       
+    imagem.src = imagemAmorSrc;
+    setTimeout(restaurarImagemOriginal, 7000);
+    setTimeout(trocarParaSlimeTriste, 20000); 
+    statusTexto.textContent = 'A imagem original foi restaurada!';
+    console.log('Imagem restaurada pelo usuário.');
+}
+
+
+function trocarParaSlimeDormindo() {       
+    imagem.src = imagemDormindoSrc;
+    setTimeout(restaurarImagemOriginal, 7000);
+    setTimeout(trocarParaSlimeTriste, 20000); 
+    statusTexto.textContent = 'A imagem original foi restaurada!';
+    console.log('Imagem restaurada pelo usuário.');
+}
       
-        setTimeout(trocarParaNovaImagem, 3000); 
-        botaoComer.addEventListener('click',restaurarImagemOriginal);
+setTimeout(trocarParaSlimeTriste, 5000); 
+
+botaoComer.addEventListener('click',trocarParaSlimeComendo);
+
+botaoCarinho.addEventListener('click',trocarParaSlimeCarinho);
+
+botaoDormir.addEventListener('click',trocarParaSlimeDormindo);
